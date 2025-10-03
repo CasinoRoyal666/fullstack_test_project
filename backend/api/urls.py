10ab3d01a05ac8edd64ca569e37efad645e24d56
CasinoRpoyal666 <danikpranik1@gmail.com>
2025-10-03 21:20:52 +0300

    Created serializers.py and urls.py in api/ project folder; Created a simple test model, serializer and view; Changed settings.py and urls.py.

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns= [
     path('test/', views.testing_endpoint, name='test'),
     path('items/', views.items_list, name='items-list'),
-    path('items/create/', views.post_item, name='post-item')
+    path('items/create/', views.post_item, name='post-item'),
+    path('items/<int:pk>/', views.item_details, name='item_details'),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -6,18 +6,28 @@ from rest_framework.request import Request
 from rest_framework.decorators import api_view
 from rest_framework import status
 
+"""
+This is a test endpoint, nothing useful
+"""
 @api_view(['GET'])
 def testing_endpoint(request):
     return Response({
         'message': 'Hello from api',
         'status': 'success'
     })
+
+"""
+Get list of items
+"""
 @api_view(['GET'])
 def items_list(request):
     items = Item.objects.all()
     serializer = ItemSerializer(items, many=True)
     return Response(serializer.data)
 
+"""
+Create a new item
+"""
 @api_view(['POST'])
 def post_item(request):
     serializer = ItemSerializer(data=request.data)
@@ -25,6 +35,35 @@ def post_item(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+"""
+Operations on a single selected item
+"""
+@api_view(['GET', 'PUT', 'DELETE'])
+def item_details(request, pk):
+    try:
+        item = Item.objects.get(pk=pk)
+    except Item.DoesNotExist:
+        return Response(
+            {'error': 'Item does not exist'},
+            status = status.HTTP_400_BAD_REQUEST
+        )
+    if request.method == 'GET':
+        serializer = ItemSerializer(item)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = ItemSerializer(item, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        item.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+
+
+
 
 
 
