4537e149c94ff3c41159e785f7b4dd1b5b62118c
CasinoRpoyal666 <danikpranik1@gmail.com>
2025-10-04 09:20:18 +0300

    Added Item model, serializer, CRUD views on Item model Added urls.py in api/ project folder Changed settings.py Added couple of API tests based on APITestCase class

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+class Item(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
-# Create your models here.
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return  self.title
+
+
+
+
+
+
+
